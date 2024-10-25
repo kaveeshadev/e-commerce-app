ec33ccbf0c6297daa6e856cb46247721740aecfd
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-10-25 22:35:45 +0530

    Set the Search Bar Logic

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(null);
+  const [image,setImage] = useState('');
 
-export default Product
+
+  const fetchProductData = () => {
+    setProductData(
+      products.find((item) => {
+        if (item._id === productId) {
+        setImage(item.image[0])
+          return true;
+        }
+        return false;
+      })
+    );
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productData ? (
+  
+  <div>
+
+  </div>
+  ) : <div className="opacity-0">
+
+  </div>
+};
+
+export default Product;

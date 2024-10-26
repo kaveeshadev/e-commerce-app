719a41df9d8451463b830335fa4df54ee6e54275
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-10-26 13:08:14 +0530

    Place Order Page Done

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -1,35 +1,41 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from './Title';
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const CartTotal = () => {
-
-const {currency,delivery_fee , getCardAmount} = useContext(ShopContext); 
+  const { currency, delivery_fee, getCardAmount } = useContext(ShopContext);
 
   return (
-    <div className='w-full'>
-      <div className='text-2xl '>
-         <Title text1={'CART'} text2={'TOTALS'} />
+    <div className="w-full">
+      <div className="text-2xl ">
+        <Title text1={"CART"} text2={"TOTALS"} />
       </div>
 
-      <div className='flex flex-col gap-2 mt-2 text-sm'>
-       <div className='flex justify-between '>
-           <p>Sub Total</p>
-           <p>{currency} {getCardAmount()}.00 </p>
-       </div>
-       <hr />
-       <div className='flex justify-between'>
-        <p>Shipping Fee</p>
-        <p>{currency} {delivery_fee}.00 </p>
-       </div>
-       <hr />
-       <div className='flex justify-between'>
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between ">
+          <p>Sub Total</p>
+          <p>
+            {currency} {getCardAmount()}.00{" "}
+          </p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <p>Shipping Fee</p>
+          <p>
+            {currency} {delivery_fee}.00{" "}
+          </p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
           <b>Total</b>
-          <b>{currency} {getCardAmount() === 0 ? 0 : getCardAmount() + delivery_fee}.00 </b> 
-       </div>
+          <b>
+            {currency}{" "}
+            {getCardAmount() === 0 ? 0 : getCardAmount() + delivery_fee}.00{" "}
+          </b>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartTotal
+export default CartTotal;

0b1d16b026869f90ed03ab983ac626cb4bf4f62b
Kaveesha Dissanayake <90174106+kaveeshadev@users.noreply.github.com>
2024-10-31 22:15:42 +0530

    UI Changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+ export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
-    <>
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
 
-     <Navbar/>
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-
-    </>
-  )
-}
-
-export default App
+export default App;

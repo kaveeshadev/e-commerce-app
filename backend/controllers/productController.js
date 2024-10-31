5a1443ceb4f6500619eeee88a9091d27dd9bf8c8
Kaveesha Dissanayake <90174106+kaveeshadev@users.noreply.github.com>
2024-10-31 13:37:06 +0530

    Finishing Create Product Controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -59,14 +59,43 @@ const addProduct = async (req, res) => {
 
 // function for list products
 
-const listProduct = async (req, res) => {};
+const listProduct = async (req, res) => {
+    try {
+        
+     const products = await productModel.find({});
+     res.json({success:true,products})
+
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message }); 
+    }
+};
 
 // function for removing products
 
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product removed"})
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });  
+    }
+};
 
 // function for single product info
 
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+    try {
+        
+       const {productId} = req.body
+       const product = await productModel.findById(productId)
+       res.json({success:true,product})
+
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });  
+    }
+};
 
 export { listProduct, addProduct, removeProduct, singleProduct };

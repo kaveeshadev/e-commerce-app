5f982b29b40d51dd585c0b710b5c68bf73cac6e5
Kaveesha Dissanayake <90174106+kaveeshadev@users.noreply.github.com>
2024-10-31 20:46:09 +0530

    Create the folder structure for Admin Panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+
+     <Navbar/>
+
+
+    </>
   )
 }
 
